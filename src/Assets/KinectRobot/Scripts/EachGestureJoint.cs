50e87761ee771fc3a2e20d05c33fdeb8fd6ff655
aripinky <aripinky@nate.com>
2014-07-25 14:08:09 +0900

    옵션기능
    공연 컨트롤

---
+++ src/Assets/KinectRobot/Scripts/EachGestureJoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EachGestureJoint : MonoBehaviour 
+{
+	public bool _bIsContact = false;
+
+
+
+	void OnTriggerStay(Collider other) 
+	{
+		if (tag == other.tag)
+			_bIsContact = true;
+		else
+			_bIsContact = false;
+	}
+
+	void OnTriggerExit(Collider other) 
+	{
+		_bIsContact = false;
+	}
+
+}
--- src/Assets/KinectRobot/Scripts/MainControl.cs
+++ src/Assets/KinectRobot/Scripts/MainControl.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 using UnityRobot;
 
@@ -10,12 +11,12 @@ using UnityRobot;
 public enum EMode
 {
 	TITLE,
+	STANDBY,
+	OPTION,
 	WAITING,
 	GESTURE,
 	AVATAR,
 }
-
-
 
 
 
@@ -35,6 +36,13 @@ public class MainControl : MonoBehaviour
 
 	// NGUI ========================
 	public GameObject		_goUI_Title;
+	public GameObject		_goPnl_Title;
+	public GameObject		_goPnl_Option;
+	public GameObject		_goPnl_Deco;
+
+	public GameObject		_goLbl_Standby;
+	public UILabel			_ULbl_StandbyTime;
+
 	public GameObject		_goUI_Waiting;
 	public GameObject		_goObj_Robot;
 
@@ -55,7 +63,33 @@ public class MainControl : MonoBehaviour
 	public UILabel			_UILblMessage;
 	public UILabel			_UILblTimer;
 	String					_strMode = "";
+
+	public UILabel			_UILblRepeat_Title;
+	public UILabel			_UILblRepeat_Waiting;
+
+	public UILabel			_UILblDuration;
 	// NGUI ========================
+
+	// OPTION & NGUI ========================
+	public int		_nOption_StartTime = 1;
+	public int		_nOption_Repeat = 1;
+	public int		_nOption_Interval = 1;
+	public int		_nOption_Duration = 1;
+	public int		_nOption_PlayTime = 10;
+	public int		_nOption_Button = 10;
+
+	public UILabel	_Ulbl_StartTime;
+	public UILabel	_Ulbl_Repeat;
+	public UILabel	_Ulbl_Interval;
+	public UILabel	_Ulbl_Duration;
+	public UILabel	_Ulbl_PlayTime;
+	public UILabel	_Ulbl_Button;
+	// OPTION & NGUI ========================
+
+
+	int _nCurRepeat = 0; // 현재반복횟수--
+	int _nCurDuration = 0;	// 회당 제한시간---
+	int _nCurInterval = 0;	// 다음공연까지의 남은시간-
 
 	int _nAvatarTimer = 0;
 
@@ -66,16 +100,25 @@ public class MainControl : MonoBehaviour
 
 
 
-	// Gesture ====================
-	public Transform	_trGuide_Arm_L;
-	public Transform	_trGuide_Hand_L;
-	public Transform	_trGuide_Arm_R;
-	public Transform	_trGuide_Hand_R;
+	int _nGestureNum = -2;
 
-	public Transform	_trCheck_Arm_L;
-	public Transform	_trCheck_Hand_L;
-	public Transform	_trCheck_Arm_R;
-	public Transform	_trCheck_Hand_R;
+	public List<GameObject> GestureList = new List<GameObject>();
+	public List<GameObject> GuideList = new List<GameObject>();
+
+	public GameObject		_goSuccessGesture;
+
+	public EachGestureJoint		_scr_Arm_L;
+	public EachGestureJoint		_scr_Hand_L;
+	public EachGestureJoint		_scr_Arm_R;
+	public EachGestureJoint		_scr_Hand_R;
+
+
+
+
+
+
+
+
 
 
 
@@ -99,14 +142,59 @@ public class MainControl : MonoBehaviour
 		GoTitle();
 
 		_AvatarJoints = FindObjectsOfType(typeof(KinectAvatarJoint)) as KinectAvatarJoint[];
+	
+		LoadOptions();
 	}
 
 
 
 
+	void LoadOptions()
+	{
+		if (PlayerPrefs.GetInt("PP_StartTime") != 0 )
+		{
+			_nOption_StartTime = PlayerPrefs.GetInt("PP_StartTime");
+			_Ulbl_StartTime.text = _nOption_StartTime.ToString();
+		}
+		if (PlayerPrefs.GetInt("PP_Repeat") != 0 )
+		{
+			_nOption_Repeat = PlayerPrefs.GetInt("PP_Repeat");
+			_Ulbl_Repeat.text = _nOption_Repeat.ToString();
+			Show_RepeatCounter();
+		}
+		if (PlayerPrefs.GetInt("PP_Interval") != 0 )
+		{
+			_nOption_Interval = PlayerPrefs.GetInt("PP_Interval");
+			_Ulbl_Interval.text = _nOption_Interval.ToString();
+		}
+		if (PlayerPrefs.GetInt("PP_Duration") != 0 )
+		{
+			_nOption_Duration = PlayerPrefs.GetInt("PP_Duration");
+			_Ulbl_Duration.text = _nOption_Duration.ToString();
+		}
+		if (PlayerPrefs.GetInt("PP_PlayTime") != 0 )
+		{
+			_nOption_PlayTime = PlayerPrefs.GetInt("PP_PlayTime");
+			_Ulbl_PlayTime.text = _nOption_PlayTime.ToString();
+		}
+		if (PlayerPrefs.GetInt("PP_Button") != 0 )
+		{
+			_nOption_Button = PlayerPrefs.GetInt("PP_Button");
+			_Ulbl_Button.text = _nOption_Button.ToString();
+		}
+	}
 
 
 
+	public void SaveOptions()
+	{
+		PlayerPrefs.SetInt("PP_StartTime",_nOption_StartTime);
+		PlayerPrefs.SetInt("PP_Repeat",_nOption_Repeat);
+		PlayerPrefs.SetInt("PP_Interval",_nOption_Interval);
+		PlayerPrefs.SetInt("PP_Duration",_nOption_Duration);
+		PlayerPrefs.SetInt("PP_PlayTime",_nOption_PlayTime);
+		PlayerPrefs.SetInt("PP_Button",_nOption_Button);
+	}
 
 
 
@@ -157,6 +245,31 @@ public class MainControl : MonoBehaviour
 
 
 
+	bool AddRepeatCounter()
+	{
+		bool bValue = true;
+
+		if (_nCurRepeat < _nOption_Repeat)
+			_nCurRepeat = _nCurRepeat + 1;
+		else
+			bValue = false;
+
+		return bValue;
+	}
+
+
+
+	void Show_RepeatCounter()
+	{
+		_UILblRepeat_Title.text = "R " + _nCurRepeat.ToString() + "/" + _nOption_Repeat.ToString();
+		_UILblRepeat_Waiting.text = "R " + _nCurRepeat.ToString() + "/" + _nOption_Repeat.ToString();
+	}
+
+
+
+
+
+
 	public void GoBack()
 	{
 		if (_eCurMode == EMode.WAITING)
@@ -171,10 +284,165 @@ public class MainControl : MonoBehaviour
 	{
 		_eCurMode = EMode.TITLE;
 		_goUI_Title.SetActive(true);
+		_goPnl_Title.SetActive(true);
+		_goPnl_Deco.SetActive(true);
+		_goLbl_Standby.SetActive(false);
+		_goPnl_Option.SetActive(false);
+		_goUI_Waiting.SetActive(false);
+		_goObj_Robot.SetActive(false);
+		_goRobot.transform.position = new Vector3(0f, 200f, 0f);
+		Show_RepeatCounter();
+	}
+
+
+	public void GoOption()
+	{
+		_eCurMode = EMode.OPTION;
+		_goUI_Title.SetActive(true);
+		_goPnl_Title.SetActive(false);
+		_goPnl_Deco.SetActive(false);
+		_goPnl_Option.SetActive(true);
 		_goUI_Waiting.SetActive(false);
 		_goObj_Robot.SetActive(false);
 		_goRobot.transform.position = new Vector3(0f, 200f, 0f);
 	}
+
+
+
+
+	public void GoQuickStart()
+	{
+		_nCurDuration = _nOption_Duration * 60; // 회당 제한 시간-
+		DurationCountDown();
+		GoWaiting();
+	}
+
+
+
+	public void GoStart()
+	{
+		if (_nOption_StartTime <= DateTime.Now.Hour)
+		{
+			if (AddRepeatCounter())
+			{
+				_nCurDuration = _nOption_Duration * 60; // 회당 제한 시간-
+				DurationCountDown();
+				GoWaiting();
+			}
+		}
+		else
+			GoStandby(true); // 시작시간을 기다림-
+	}
+
+
+
+
+	public void GoStandby(bool p_IsFirstStart)
+	{
+		_eCurMode = EMode.STANDBY;
+		_goUI_Title.SetActive(true);
+		_goPnl_Title.SetActive(false);
+		_goPnl_Deco.SetActive(true);
+		_goLbl_Standby.SetActive(true);
+		_goPnl_Option.SetActive(false);
+		_goUI_Waiting.SetActive(false);
+		_goObj_Robot.SetActive(false);
+		_goRobot.transform.position = new Vector3(0f, 200f, 0f);
+		if (p_IsFirstStart) // 처음 시작에 의한 것인가?---
+		{
+			CancelInvoke("Show_StanbyTime");
+			Invoke("Show_StanbyTime", 1f);
+		}
+		else // 공연을 끝내고 다음공연을 기다림--
+		{
+			_nCurInterval = _nOption_Interval * 60;
+			CancelInvoke("Show_ImtervalTime");
+			Invoke("Show_ImtervalTime", 1f);
+		}
+	}
+
+
+	// 시작시간을 기다림--------------------------------------------------------
+	void Show_StanbyTime()
+	{
+		if (_nOption_StartTime <= DateTime.Now.Hour)
+		{
+			if (AddRepeatCounter())
+			{
+				_nCurDuration = _nOption_Duration * 60; // 회당 제한 시간-
+				DurationCountDown();
+				GoWaiting();
+			}
+			return;
+		}
+
+		if (_nCurRepeat >= _nOption_Repeat)
+		{
+			_ULbl_StandbyTime.text = "CLOSED";
+			return;
+		}
+
+		int nTime = _nOption_StartTime - DateTime.Now.Hour - 1;
+		string strTime = string.Format("{0:D2}", nTime);
+
+		int nMinute = 60 - DateTime.Now.Minute - 1;
+		string strMinute = string.Format("{0:D2}", nMinute);
+
+		int nSecond = 60 - DateTime.Now.Second - 1;
+		string strSecond = string.Format("{0:D2}", nSecond);
+
+		_ULbl_StandbyTime.text = strTime + "." + strMinute + "." + strSecond;
+
+		Invoke("Show_StanbyTime", 1f);
+	}
+
+
+	// 다음공연을 기다림-----------------------------------------------------------
+	void Show_ImtervalTime()
+	{
+		if (_nCurInterval <= -1)
+		{
+			if (AddRepeatCounter())
+			{
+				_nCurDuration = _nOption_Duration * 60; // 회당 제한 시간-
+				DurationCountDown();
+				GoWaiting();
+			}
+			return;
+		}
+		
+		if (_nCurRepeat >= _nOption_Repeat)
+		{
+			_ULbl_StandbyTime.text = "CLOSED";
+			return;
+		}
+
+
+
+		int nTime = (int)Mathf.Floor(_nCurInterval / 360);
+		string strTime = string.Format("{0:D2}", nTime);
+		
+		int nMinute = (int)Mathf.Floor((_nCurInterval - (nTime * 360)) / 60);
+		string strMinute = string.Format("{0:D2}", nMinute);
+		
+		int nSecond = _nCurInterval - (nTime * 360) - (nMinute * 60);
+		string strSecond = string.Format("{0:D2}", nSecond);
+		
+		_ULbl_StandbyTime.text = strTime + "." + strMinute + "." + strSecond;
+
+		_nCurInterval --;
+
+		Invoke("Show_ImtervalTime", 1f);
+	}
+
+
+
+
+
+
+
+
+
 
 
 	public void GoWaiting()
@@ -186,12 +454,40 @@ public class MainControl : MonoBehaviour
 		_goWaitingSet.SetActive(true);
 		_goGestureBtnSet.SetActive(false);
 		_goObj_Robot.SetActive(true);
+		Clear_Guide();
+		_goSuccessGesture.SetActive(false);
 		CancelInvoke("CheckPlayer");
+		CancelInvoke("AvatarCountDown");
 		_UILblTimer.enabled = false;
 		CheckPlayer();
 
 		FollowOnOff(false);
+		Show_RepeatCounter();
 	}
+
+
+	void DurationCountDown()
+	{
+		if (_nCurDuration <= -1)
+		{
+			GoStandby(false); // 다음공연까지의 시간을 기다림-
+			return;
+		}
+		
+		_UILblDuration.text = "D " + _nCurDuration.ToString();
+		_nCurDuration --;
+
+		CancelInvoke("DurationCountDown");
+		Invoke("DurationCountDown", 1f);
+	}
+
+
+
+
+
+
+
+
 
 
 	public void GoGesture()
@@ -205,35 +501,81 @@ public class MainControl : MonoBehaviour
 		_goObj_Robot.SetActive(true);
 		_strMode = "Gesture Mode";
 		_UILblMessage.text = _strMode;
+		_UILblTimer.enabled = true;
+		_nAvatarTimer = _nOption_PlayTime;
+		AvatarCountDown();
+		_nGestureNum = -2;
+		SelectGesture(-1);
 	}
 
 
 
 	public void SelectGesture(int p_Num)
 	{
-		if (p_Num == -1)
-		{
-			foreach (GameObject Btn in _goGestureBtn)
-			{
-				Btn.SetActive(true);
-			}
+		if (p_Num == _nGestureNum)
 			return;
-		}
 
-		_goCursor.SetActive(false);
+		_nGestureNum = p_Num;
 
 		foreach (GameObject Btn in _goGestureBtn)
 		{
 			Btn.SetActive(false);
 		}
 
+
+		if (p_Num == -1)
+		{
+			for (int i = 0; i < _nOption_Button; i ++)
+			{
+				_goGestureBtn[i].SetActive(true);
+			}
+			_goSuccessGesture.SetActive(false);
+			Clear_Guide();
+			return;
+		}
+
+		_goCursor.SetActive(false);
+
 		_goGestureBtn[p_Num].SetActive(true);
+		GuideList[p_Num].SetActive(true);
 
-		_CMDModule.Value = (Byte)(p_Num + 1);
+		SetGestureGuide(p_Num);
 
-
-		CheckGestureEnd();
+		CheckGesture();
 	}
+
+
+
+	void Clear_Guide()
+	{
+		foreach (GameObject Img in GuideList)
+		{
+			Img.SetActive(false);
+		}
+
+	}
+
+
+
+
+
+	void CheckGesture()
+	{
+		if ( (_scr_Arm_L._bIsContact) && (_scr_Hand_L._bIsContact) && (_scr_Arm_R._bIsContact) && (_scr_Hand_R._bIsContact) )
+		{
+			_goSuccessGesture.SetActive(true);
+			Clear_Guide();
+			_CMDModule.Value = (Byte)(_nGestureNum + 1);
+			CheckGestureEnd();
+		}
+		else
+		{
+			Invoke("CheckGesture", 0.1f);
+		}
+	}
+
+
+
 
 
 	void CheckGestureEnd()
@@ -245,10 +587,15 @@ public class MainControl : MonoBehaviour
 	}
 
 
+
+
+
 	void ResetGestureBtn()
 	{
 		_goCursor.SetActive(true);
 		SelectGesture(-1);
+		_goSuccessGesture.SetActive(false);
+		Clear_Guide();
 	}
 
 
@@ -267,7 +614,7 @@ public class MainControl : MonoBehaviour
 		_strMode = "Avatar Mode";
 		_UILblMessage.text = _strMode;
 		_UILblTimer.enabled = true;
-		_nAvatarTimer = 10;
+		_nAvatarTimer = _nOption_PlayTime;
 		AvatarCountDown();
 
 		FollowOnOff(true);
@@ -284,9 +631,7 @@ public class MainControl : MonoBehaviour
 		}
 
 		_UILblTimer.text = _nAvatarTimer.ToString();
-
-//		if (_KinectManager.IsPlayerCalibrated(_KinectManager.GetPlayer1ID()))
-//			_nAvatarTimer --;
+		_nAvatarTimer --;
 
 		CancelInvoke("AvatarCountDown");
 		Invoke("AvatarCountDown", 1f);
@@ -310,6 +655,19 @@ public class MainControl : MonoBehaviour
 
 
 
+	// SetGestureGuide ----------------------------------------------------------
+	void SetGestureGuide(int p_Number)
+	{
+		foreach(GameObject goGesture in GestureList)
+		{
+			goGesture.SetActive(false);
+		}
+
+		GestureList[p_Number].SetActive(true);
+	}
+
+
+
 
 
 
@@ -330,6 +688,7 @@ public class MainControl : MonoBehaviour
 		}
 		else
 		{
+			ResetGestureBtn();
 			_goLinePerson.SetActive(true);
 			_goRobot.transform.position = new Vector3(0f, 200f, 0f);
 			_UILblMessage.text = "Waiting Player";
@@ -337,6 +696,167 @@ public class MainControl : MonoBehaviour
 		
 		Invoke("CheckPlayer", 0.1f);
 	}
+
+
+
+
+
+
+	// Option StartTime ====================================================
+
+	public void Option_SetStartTime_Plus()
+	{
+		_nOption_StartTime ++;
+
+		if (_nOption_StartTime > 24)
+			_nOption_StartTime = _nOption_StartTime - 24;
+		
+		_Ulbl_StartTime.text = _nOption_StartTime.ToString();
+	}
+
+	public void Option_SetStartTime_Minus()
+	{
+		_nOption_StartTime --;
+		
+		if (_nOption_StartTime <= 0)
+			_nOption_StartTime = _nOption_StartTime + 24;
+		
+		_Ulbl_StartTime.text = _nOption_StartTime.ToString();
+	}
+
+
+
+
+
+
+	// Option Repeat ====================================================
+
+	public void Option_SetRepeat_Plus()
+	{
+		_nOption_Repeat ++;
+		
+		if (_nOption_Repeat > 30)
+			_nOption_Repeat = _nOption_Repeat - 30;
+		
+		_Ulbl_Repeat.text = _nOption_Repeat.ToString();
+		Show_RepeatCounter();
+	}
+	
+	public void Option_SetRepeat_Minus()
+	{
+		_nOption_Repeat --;
+		
+		if (_nOption_Repeat <= 0)
+			_nOption_Repeat = _nOption_Repeat + 30;
+		
+		_Ulbl_Repeat.text = _nOption_Repeat.ToString();
+		Show_RepeatCounter();
+	}
+
+
+
+	// Option Interval ====================================================
+
+	public void Option_SetInterval_Plus()
+	{
+		_nOption_Interval ++;
+		
+		if (_nOption_Interval > 120)
+			_nOption_Interval = _nOption_Interval - 120;
+		
+		_Ulbl_Interval.text = _nOption_Interval.ToString();
+	}
+	
+	public void Option_SetInterval_Minus()
+	{
+		_nOption_Interval --;
+		
+		if (_nOption_Interval <= 0)
+			_nOption_Interval = _nOption_Interval + 120;
+		
+		_Ulbl_Interval.text = _nOption_Interval.ToString();
+	}
+
+
+
+
+
+	// Option Duration ====================================================
+
+	public void Option_SetDuration_Plus()
+	{
+		_nOption_Duration ++;
+		
+		if (_nOption_Duration > 60)
+			_nOption_Duration = _nOption_Duration - 60;
+		
+		_Ulbl_Duration.text = _nOption_Duration.ToString();
+	}
+	
+	public void Option_SetDuration_Minus()
+	{
+		_nOption_Duration --;
+		
+		if (_nOption_Duration <= 0)
+			_nOption_Duration = _nOption_Duration + 60;
+		
+		_Ulbl_Duration.text = _nOption_Duration.ToString();
+	}
+
+
+
+	// Option PlayTime ====================================================
+
+	public void Option_SetPlayTime_Plus()
+	{
+		_nOption_PlayTime = _nOption_PlayTime + 10;
+		
+		if (_nOption_PlayTime > 300)
+			_nOption_PlayTime = _nOption_PlayTime - 300;
+		
+		_Ulbl_PlayTime.text = _nOption_PlayTime.ToString();
+	}
+	
+	public void Option_SetPlayTime_Minus()
+	{
+		_nOption_PlayTime = _nOption_PlayTime - 10;
+		
+		if (_nOption_PlayTime <= 0)
+			_nOption_PlayTime = _nOption_PlayTime + 300;
+		
+		_Ulbl_PlayTime.text = _nOption_PlayTime.ToString();
+	}
+
+
+
+
+	// Option Button Number ==============================================
+
+	public void Option_SetButton_Plus()
+	{
+		_nOption_Button ++;
+		
+		if (_nOption_Button > 10)
+			_nOption_Button = _nOption_Button - 10;
+		
+		_Ulbl_Button.text = _nOption_Button.ToString();
+	}
+	
+	public void Option_SetButton_Minus()
+	{
+		_nOption_Button --;
+		
+		if (_nOption_Button <= 0)
+			_nOption_Button = _nOption_Button + 10;
+		
+		_Ulbl_Button.text = _nOption_Button.ToString();
+	}
+
+
+
+
+
+
 
 
 
